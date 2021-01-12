d6cc84d0f2316b7c25811846dd2ad38d5a2ec98a
kaede.togasaki <rosso.e.verde1014@gmail.com>
2021-01-12 19:25:01 +0900

    Modify null control

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -31,9 +31,11 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("task", m);
         request.setAttribute("_token", request.getSession().getId());
 
+        if(m != null){
         request.getSession().setAttribute("task_id", m.getId());
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
+        }
     }
 }
